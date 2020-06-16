87e7f2b8353760ed44e00c2c56c59d32f3e5cfc1
Cheikh Gueye Wane <cheikhgwane@gmail.com>
2020-06-16 20:30:42 +0000

    fix elmo endpoint

---
+++ app/src/elmo.py
@@ -0,0 +1,122 @@
+import os
+import json
+import keras
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+import gensim
+import sys
+
+from keras.models import Model, Input
+from keras.layers.merge import add
+from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional, Lambda
+from keras import backend as K
+
+batch_size = 35
+max_len = 117
+
+tf.compat.v1.disable_v2_behavior()
+
+
+
+""" session = K.get_session()
+
+init = tf.global_variables_initializer()
+session.run(init)
+
+graph = tf.get_default_graph() """
+
+BASE_PATH = os.path.abspath(os.path.join(__file__, "../../.."))
+
+sys.path.append(BASE_PATH+"/app/src/")
+from utils import loadFile, pre_process_doc, post_process, dataCleaning,divide_chunks,fill_out,prepare
+##################
+model_path = BASE_PATH + "/models/saved_model/lstm_elmo.h5"
+
+phraser_path = BASE_PATH + "/models/saved_model/phraser"
+
+tag2idx_path = BASE_PATH + "/app/assets/tag.json"
+
+phraser_path = BASE_PATH + "/models/saved_model/phraser"
+
+phraser = gensim.models.Phrases.load(phraser_path)
+
+with open(tag2idx_path, encoding="utf-8") as f:
+    tag2idx = json.load(f)
+
+tags = tag2idx.keys()
+tags = list(tag2idx.keys())
+n_tags = len(tags)
+
+
+def loadElmo():
+    sess = K.get_session()
+    K.set_session(sess)
+    elmo_model = hub.Module("https://tfhub.dev/google/elmo/3", trainable=False)
+    sess.run(tf.tables_initializer())
+    return elmo_model
+
+
+def ElmoEmbedding(x):
+    elmo_model = loadElmo()
+    return elmo_model(inputs={
+        "tokens": tf.squeeze(tf.cast(x, tf.string)),
+        "sequence_len": tf.constant(batch_size*[max_len])
+    },
+        signature="tokens",
+        as_dict=True)["elmo"]
+
+
+def createModel():
+    input_text = Input(shape=(max_len,), dtype="string")
+    embedding = Lambda(ElmoEmbedding, output_shape=(None, 1024))(input_text)
+    x = Bidirectional(LSTM(units=512, return_sequences=True,
+                           recurrent_dropout=0.2, dropout=0.3))(embedding)
+    x_rnn = Bidirectional(
+        LSTM(units=512, return_sequences=True, recurrent_dropout=0.2, dropout=0.3))(x)
+    x = add([x, x_rnn])  # residual connection to the first biLSTM
+    out = TimeDistributed(Dense(n_tags, activation="softmax"))(x)
+    model = Model(input_text, out)
+    return model
+
+
+def elmoPredict(filepath):
+    model = createModel()
+    model.load_weights(model_path)
+    ##model.summary()
+
+    raw_file = loadFile(filepath)
+    cleaned_file = dataCleaning(raw_file)
+
+    res = list(divide_chunks(cleaned_file, max_len))
+    pred_dict = {}
+    for r in res:
+        sent = prepare(r, batch_size, max_len)
+        offset = batch_size - len(sent)
+        for i in range(offset):
+            sent.append(fill_out([]))
+        p = model.predict(np.array(sent),verbose=1, batch_size=35)[0]
+
+        p = np.argmax(p, axis=-1)
+        i = 0
+        for w, pred in zip(sent[0], p):
+            pred_dict[str(i)+'_'+w] = tags[pred]
+            i += 1
+            #print("{:15}: {:5}".format(w, tags[pred]))
+    return post_process(pred_dict)
+  
+
+
+def prepareSentence(sentence):
+    new_seq = []
+    for i in range(max_len):
+        try:
+            new_seq.append(sentence[i])
+        except:
+            new_seq.append("__PAD__")
+    return new_seq
+
+
+if __name__ == "__main__":
+    test_file = r"C:\Users\Cheikh\Desktop\Projet_memoire\myArmAi\samples\base_cv\cv\CV ATOS Amadou NDIAYE - ENGLISH.docx"
+    elmoPredict(test_file)
--- app/src/predict.py
+++ app/src/predict.py
@@ -1,22 +1,15 @@
-from keras.preprocessing.sequence import pad_sequences
-from tensorflow.python.keras.backend import set_session
-from tensorflow.python.keras.models import load_model
-
-from collections import Counter
-
-import math
-import textract
 import os
-import re
 import json
-import numpy as np
 import tensorflow as tf
 import keras
 import gensim
-import nltk
+import numpy as np
+import sys
+import sys
 
-import string
-printable = set(string.printable)
+from tensorflow.python.keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+from tensorflow.python.keras.backend import set_session
 
 tf.compat.v1.disable_v2_behavior()
 
@@ -24,15 +17,15 @@ session = keras.backend.get_session()
 init = tf.global_variables_initializer()
 
 session.run(init)
-
 graph = tf.get_default_graph()
 
 BASE_PATH = os.path.abspath(os.path.join(__file__, "../../.."))
 
+sys.path.append(BASE_PATH+"/app/src/")
+from utils import loadFile, pre_process_doc, post_process, dataCleaning
+
 ##################
 model_path = BASE_PATH + "/models/saved_model/lstm_ner_model_F1_37_3.h5"
-
-phraser_path = BASE_PATH + "/models/saved_model/phraser"
 
 word2idx_path = BASE_PATH + "/app/assets/word2idx.json"
 
@@ -40,8 +33,6 @@ tag2idx_path = BASE_PATH + "/app/assets/tag2idx.json"
 ##################
 
 model = tf.keras.models.load_model(model_path)
-
-phraser = gensim.models.Phrases.load(phraser_path)
 
 word2idx = {}
 max_len = 50
@@ -53,59 +44,12 @@ with open(tag2idx_path, encoding="utf-8") as f:
     tag2idx = json.load(f)
 
 tags = tag2idx.keys()
-
 tags = list(tag2idx.keys())
 
-
-def loadFile(filepath):
-
-    if not os.path.isfile(filepath):
-        raise Exception("OpenFileException", "File doesn't exist")
-
-    raw_file = textract.process(os.path.join(filepath)).decode()
-    return raw_file
-
-
-def dataCleaning(raw_data, stop_lang="english"):
-    r = re.sub("[\\r\\t\\n\|\-/]", " ", raw_data)
-    #r = ''.join(filter(lambda x: x in printable, r))
-    r = r.split()
-    return r
-
-
-def pre_process_doc(data):
-    array_nbr = math.ceil(len(data)/max_len)
-    sequences = np.array_split(np.array(data), array_nbr)
-    return sequences
-
-
-def post_process(out):
-    # if a number follow a skills --> transform to level
-    # if a string has a duration has a tag --> O
-    ##
-    # for key,value in out.items():
-    category = []
-    for key, value in out.items():
-        category.append(value)
-        if value == "Skills" and re.match("[0-9]", key.split("_")[1]):
-            out[key] = "Level"
-    bi_gram = []
-    result = {}
-    category = list(set(category))
-    for _c in category:
-        c = [x.split("_")[1].lower() for x, y in out.items() if y == _c]
-        # apply bigram model
-        result[_c] = phraser[c]
-    return result
-
-
 def predict(filepath):
-
-   # print(filepath)
     raw_test_file = loadFile(filepath)
 
     cleaned_test_file = dataCleaning(raw_test_file)
-
     sequences = pre_process_doc(cleaned_test_file)
     pred_dict = {}
 
@@ -126,17 +70,18 @@ def predict(filepath):
                 pred_dict[str(i)+'_'+w] = tags[pred]
                 output_file += w+"  "+tags[pred]+"\n"
                 i += 1
-                #print("{:15}: {:5}".format(w, tags[pred]))
-
-    # print(pred_dict)
-    return post_process(pred_dict)
+                #print("{:15}: {:5}".format(w, pred))
+                
+    res = post_process(pred_dict)
+    #print(res)
+    return res
 
 
 if __name__ == "__main__":
 
     print("main")
 
-    test_file = r'C:\Users\Cheikh\Desktop\Projet_memoire\myArmAi\samples\base_cv\cv\CV ATOS Fassou Mathias Niamy - ENGLISH.docx'
+    test_file = r'C:\Users\Cheikh\Desktop\Projet_memoire\myArmAi\samples\base_cv\cv\CV ATOS Amadou NDIAYE - ENGLISH.docx'
     r = predict(test_file)
     print(r)
     #res = predict("./out/CV_ATOS_Amadou_NDIAYE_-_ENGLISH.doc")
